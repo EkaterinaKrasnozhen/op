47ef8375e1de0de137e33705b84f723d04849b6a
kate <01redcat@mail.ru>
2022-09-25 20:51:59 +0300

    hw 2 done& C:/Users/медведь/AppData/Local/Programs/Python/Python310/python.exe -m pip install -U autopep8

---
+++ Python/homework_seminar1/2.py
@@ -0,0 +1,15 @@
+#Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+x = int(input('Введите число x '))
+y = int(input('Введите число y '))
+z = int(input('Введите число z '))
+
+def true_or(l,k,m):
+    a = not (l or k or m)
+    b = not l and not k and not m
+    res = a == b
+    if (res == True):
+        print('истинно')
+    else:
+        print('ложно')
+
+true_or(x,y,z)
