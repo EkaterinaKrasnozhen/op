e4ed13004d546386ac6f026951507da267d5b66d
kate <01redcat@mail.ru>
2022-09-26 21:32:11 +0300

    formatting

--- Python/homework_seminar1/2.py
+++ Python/homework_seminar1/2.py
@@ -1,7 +1,6 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
 
-
 def true_or(l, k, m):
     a = not (l or k or m)
     b = not l and not k and not m
@@ -10,13 +9,11 @@ def true_or(l, k, m):
         print('истинно')
     else:
         print('ложно')
+
+
 for x in range(2):
     for x in range(2):
         for y in range(2):
             for z in range(2):
                 print(f' x {x} y {y} z {z}')
                 (true_or(x, y, z))
-
-
-
-
