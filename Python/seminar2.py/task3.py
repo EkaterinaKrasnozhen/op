0fe098de860be2814d464a8c90f574f05afd46ef
kate <01redcat@mail.ru>
2022-09-28 22:07:15 +0300

    task3 done, нахождение совпадений в строках

--- Python/seminar2.py/task3.py
+++ Python/seminar2.py/task3.py
@@ -1,16 +1,21 @@
-# 3.Напишите программу, в которой пользователь будет задавать две строки, 
+# 3.Напишите программу, в которой пользователь будет задавать две строки,
 # а программа - определять количество вхождений одной строки в другой.
 a_str = input('Введите строку а : ')
 b_str = input('Введите строку b : ')
 
-def coincid(a,b):
-    count=0
-    for i in a:
-        for e in b:
-            if i==e:
-                count+=1
-    return count
 
-print(coincid(a_str,b_str))
+def coincid(a, b):
+    res = a_str.count(b_str)
+    return res
 
-#print(a_str.count(b_str))
+
+print(coincid(a_str, b_str))
+
+
+def coincid2(a_st, b_st):
+    index = 0
+    if b_st in a_st:
+        index += 1
+    return index
+
+print(coincid2(a_str,b_str))
