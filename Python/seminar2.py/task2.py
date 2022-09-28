cfe8eb6e564d220099af85afcec3a44c8764542d
kate <01redcat@mail.ru>
2022-09-28 20:51:00 +0300

    task2 done, print(*result) без [],

---
+++ Python/seminar2.py/task2.py
@@ -0,0 +1,16 @@
+# 2.Для натурального n создать список, состоящий из элементов последовательности 3n + 1.
+#     Пример:
+#     - Для n = 6: [4, 7, 10, 13, 16, 19]
+from queue import Empty
+
+
+num = int(input('Введите натуральное число '))
+
+def n_elements(n):
+    result = []
+    for i in range(1,n+1):
+        res = 3*i+1
+        result.append(res)
+    print (result[:]) # если поставить (*result[:]) то будет печать без [] и ,
+
+n_elements(num)
