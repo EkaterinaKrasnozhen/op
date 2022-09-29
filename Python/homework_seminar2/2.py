d1fa050455303827ce2cc2f7b5baffd2141aa434
kate <01redcat@mail.ru>
2022-09-29 21:24:20 +0300

    2.py произведения от 1 до N

--- Python/homework_seminar2/2.py
+++ Python/homework_seminar2/2.py
@@ -3,5 +3,16 @@
 
 num = int(input('Введите число N '))
 
+
 def multi_N(n):
-    
+    result = [1, ]
+    if n == 0:
+        print(0)
+    else:
+        for i in range(2, n+1):
+            res = i*result[i-2]
+            result.append(res)
+        print(result)
+
+
+multi_N(num)
