44b7472fef742b056644b03f154836f0ad1cd03f
kate <01redcat@mail.ru>
2022-09-30 09:59:25 +0300

    4.py -n,n печать из positions, вывод в одну строку с пробелом

--- Python/homework_seminar2/4.py
+++ Python/homework_seminar2/4.py
@@ -1,17 +1,20 @@
-# 4. Задать список из N элементов, заполненных числами из [-N, N]. 
-# Найти произведение элементов на указанных позициях. 
+# 4. Задать список из N элементов, заполненных числами из [-N, N].
+# Найти произведение элементов на указанных позициях.
 # Позиции хранятся в списке positions - создайте этот список (например: positions = [1, 3, 6]).
 
 number = int(input('Введите число N '))
-positions = [1,3,6]
+positions = [1, 3, 6]
+
+
 def multi_position_list(n):
     if n < 0:
-        n=n*-1
+        n = n*-1
     result = []
     for i in range(-n, n+1):
-            result.append(i)
+        result.append(i)
     return result
+
+
 numbers = multi_position_list(number)
 for i in positions:
-   # print (numbers[i], end='', )
-    print ('{} '.format(numbers[i]), end='')
+    print('{} '.format(numbers[i]), end='')
