d3dd52915435a905f84c58fdf721b496a26c4ed2
kate <01redcat@mail.ru>
2022-09-30 16:00:02 +0300

    5.py перемешивание списка

--- Python/homework_seminar2/5.py
+++ Python/homework_seminar2/5.py
@@ -1,16 +1,15 @@
 # 5. Реализовать алгоритм перемешивания списка.
 
-text = ['красный', '56', 'нет', 'игра']
+text = ['красный', '56', 'нет', 'игра', 'слава']
 
 
 def mash_list(my_list):
-    
-    for i in range(0, len(my_list)-1):
+    len_half = (len(my_list)-1)//2
+    print(len_half)
+    for i in range(0, len_half):
         temp = my_list[len(my_list)-1-i]
         my_list[len(my_list)-1-i] = my_list[i]
         my_list[i] = temp
-# не пойму, почему дальше не идет перемешивание, а только крайние элементы?
     print(my_list)
-
 
 mash_list(text)
