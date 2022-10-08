df548316617a9b9feb2c96a45a72a41b89c5f1d9
kate <01redcat@mail.ru>
2022-10-08 22:10:06 +0300

    h/w 2.py разложить число на простые множители

--- Python/homework_seminar4/2.py
+++ Python/homework_seminar4/2.py
@@ -1,19 +1,21 @@
-# 2. Задайте натуральное число N. Напишите программу, 
+# 2. Задайте натуральное число N. Напишите программу,
 # которая составит список простых множителей числа N.
 
-num = int(input('Введите натуральное число: '))
+num = int(input("Введите натуральное число: "))
+
 
 def nat_divider(n):
     div_2 = 2
     lis = []
-    while div_2*div_2<=n:
-    
-        while n%div_2 == 0:
-          n//=div_2         # вот до этого момента сама додумалась
-          lis.append(div_2)
-        div_2+=1  # это подсмотрела
-    if n>1:
+    while div_2 * div_2 <= n:
+
+        while n % div_2 == 0:
+            n //= div_2  # вот до этого момента сама додумалась
+            lis.append(div_2)
+        div_2 += 1  # это подсмотрела
+    if n > 1:
         lis.append(n)
     return lis
-    
-print(*nat_divider(num))   
+
+
+print(*nat_divider(num))
