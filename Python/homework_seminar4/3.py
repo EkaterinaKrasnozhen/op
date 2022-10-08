256d4de1fa2d601e1cb947210e8a4932eb1a6bc5
kate <01redcat@mail.ru>
2022-10-08 08:42:40 +0300

    h/w 3.py todo

--- Python/homework_seminar4/3.py
+++ Python/homework_seminar4/3.py
@@ -2,22 +2,30 @@
 # Напишите программу, которая выведет список неповторяющихся
 # элементов исходной последовательности.
 
-my_list = [3, 3, 8, 3, 54, 5]
+my_list = [3, 3, 8, 3, 54, 5, 3, 54]
 
 
 def unic_elem(a):
     index = 0
-    while index < len(a):
-        for i in a:
-            if int(a[index]) == int(i):
-                a.pop(index)
-                a.remove(i)
-                print(a)
-            
-            else:
-                index+1              
+    b = []
+    c = a
+    while index < len(c)-1:
+        for i in range(1, len(a)-1):
 
-    print(a)
+            if c[index] == a[i]:
+                print(f" с[{index}] = {c[index]}, a[{i}] = {a[i]}")
+                b.append(index)
+                b.append(i)
+
+                print(f" index {index}")
+                i+=1
+            else:
+                index += 1
+            
+        
+    list_index = set(b)
+
+    print(list_index)
 
 
 unic_elem(my_list)
