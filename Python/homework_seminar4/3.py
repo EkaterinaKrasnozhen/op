a6c1d5dd371dc23e6fd46d4142694da5b0e35967
kate <01redcat@mail.ru>
2022-10-08 09:05:14 +0300

    h/w 3.py множества, убрать повторяющиеся элементы

--- Python/homework_seminar4/3.py
+++ Python/homework_seminar4/3.py
@@ -2,31 +2,11 @@
 # Напишите программу, которая выведет список неповторяющихся
 # элементов исходной последовательности.
 
-my_list = [3, 3, 8, 3, 54, 5, 3, 54]
+my_list = [3, 8, 3, 54, 5, 54]
 
 
 def unic_elem(a):
-    index = 0
-    b = []
-    c = a
-    while index < len(c)-1:
-        for i in range(1, len(a)-1):
-
-            if c[index] == a[i]:
-                print(f" с[{index}] = {c[index]}, a[{i}] = {a[i]}")
-                b.append(index)
-                b.append(i)
-
-                print(f" index {index}")
-                i+=1
-            else:
-                index += 1
-            
-        
-    list_index = set(b)
-
-    print(list_index)
-
+    b = set(a.copy())
+    print(b)
 
 unic_elem(my_list)
-# почему не сравниваются дальше индексы?
