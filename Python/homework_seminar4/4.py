df27ee8048b5ec2de41370f4a1116b2bdb7c0c3c
kate <01redcat@mail.ru>
2022-10-09 10:33:20 +0300

    h/w 4.py уравнение многочлен

--- Python/homework_seminar4/4.py
+++ Python/homework_seminar4/4.py
@@ -1,27 +1,23 @@
-# 4. Задана натуральная степень k. Сформировать случайным образом список коэффициентов 
+# 4. Задана натуральная степень k. Сформировать случайным образом список коэффициентов
 # (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 # 	Пример:
 # 	k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
-from lib2to3.pygram import Symbols
+
 import random
 
+degree_num = 2
 
 
-input_x = symbol('x')
-print(input_x)
-degree_num = 4
+def polynom(k):
+    x = []
+ 
+    for i in range(0, k - 1):
+        x.append(f"{random.randint(0,100)}x^{k-i} + ")
+    x.append(f"{random.randint(0,100)}x + ")
+    x.append(f"{random.randint(0,100)} = 0")
+    a = "".join(str(n) for n in x)
 
-def polynom(k,x):
-    lis = []
-    num = random.randint(0,100)
-    print(num)
-    for i in range(1,k+1):
-        
-        lis.append(i)
-    print(lis)
-    
-    
-        
-    
-print(polynom(degree_num, input_x))
+    print(a)
+
+polynom(degree_num)
