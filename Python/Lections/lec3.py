6a218def9152a65cf5aa9858569596d9ae938c49
kate <01redcat@mail.ru>
2022-10-10 13:06:47 +0300

    lec3.py lambda, filter, map, zip, enumerate, list comprehension

--- Python/Lections/lec3.py
+++ Python/Lections/lec3.py
@@ -77,20 +77,94 @@
 
 
 #анонимные лямбда функции
+# with open("lec3.txt", "a") as data:
+#      data.write("1 2 3 5 8 15 23 38")
 
-f = open('f.txt', 'r')
-data = f.read() + ' '
-f.close()
 
-numbers = []
+# path = 'lec3.txt'
+# f = open(path, 'r')
+# data = f.read() + ' ' #добавляем пробел
+# f.close()
 
-while data != '':
-    space_pos = data.index(' ')
-    numbers.append(int(data[:space_pos]))
-    data = data[space_pos+1:]
+# numbers = []
+
+# while data != '':
+#     space_pos = data.index(' ') # сохраняем позицию первого пробела
+#     numbers.append(int(data[:space_pos]))
+#     data = data[space_pos+1:]
     
-out = []
-for e in numbers:
-    if not e%2:
-        out.append((e,e**2))
-print(out)
+# out = []
+# for e in numbers:
+#     if not e%2:
+#         out.append((e,e**2))
+# print(out) # программа выполняет задание с лекции: четные значения возвести в квадрат, считывает из файла
+
+# сократить с помощью лямбды
+# def select (f,col):
+#     return [f(x) for x in col]
+
+# def where (f,col):
+#     return [x for x in col if f(x)]
+
+# data = '1 2 3 5 8 15 23 38'.split()
+
+# res = select(int, data)
+# res = where(lambda x: not x % 2, res)
+# res  = select(lambda x:(x, x**2), res)
+# print(res)
+
+# map
+# li = [x for x in range(1,20)]
+# li = list(map(lambda x:x+10, li))
+# print(li)
+
+# data = list(map(int,input().split())) #по умолчанию разделитель пробел
+# print(data)
+
+# data = map(int,'1 2 3 4'.split()) # без приведения к типу list получим итераторы, по ним можно пройтись только один раз
+# for e in data:
+#     print(e)
+
+# data = '1 2 3 5 8 15 23 38'.split()
+# def where (f,col):
+#     return [x for x in col if f(x)]
+# # убрали функцию select и заменили на map
+# res = map(int, data)
+# res = where(lambda x: not x % 2, res)
+# res  = list(map(lambda x:(x, x**2), res))
+# print(res)
+
+# filter (Замена where)
+
+# data = [x for x in range(10)]
+
+# res  = list(filter(lambda x: not x%2, data)) # not x%2 четные, x%2 не четные (тк == 1, true)
+# print(res)
+# #земенили where на filter
+# data = '1 2 3 5 8 15 23 38'.split()
+# res = map(int, data)
+# res = filter(lambda x: not x % 2, res)
+# res  = list(map(lambda x:(x, x**2), res))
+# print(res)
+
+# zip
+
+# users = ['user1', 'user2', 'user3', 'user5']
+# ids = [4,5,9,14,7]
+# data = list(zip(users,ids))
+# print(data) # сопоставит 1 из users с 1 ids и тд
+
+# users = ['user1', 'user2', 'user3', 'user5']
+# ids = [4,5,9,14,7]
+# salary = [111,22,33]
+# data = list(zip(users,ids, salary)) # сопоставит только для трех первых эелементов из каждого списка
+# print(data) # получим кортежи
+
+
+# enumerate  - на выходе кортежи с пронумерованными элементами
+
+users = ['user1', 'user2', 'user3', 'user5']
+ids = [4,5,9,14,7]
+salary = [111,22,33]
+data = list(enumerate(ids)) # пронумеровать
+print(data) # получим кортежи
