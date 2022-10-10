3de5c1c8a6f6f1a033bd6d5d109e039a8b94f9fc
kate <01redcat@mail.ru>
2022-10-10 11:55:19 +0300

    h/w 3.py уникальные элементы из списка

--- Python/homework_seminar4/3.py
+++ Python/homework_seminar4/3.py
@@ -4,8 +4,21 @@
 
 my_list = [3, 8, 3, 54, 5, 54]
 
+
 def unic_elem(a):
     b = set(a.copy())
     print(b)
 
+
 unic_elem(my_list)
+
+
+def unic_elem_2(b):
+    uniq_list = []
+    for i in my_list:
+        if my_list.count(i) == 1:
+            uniq_list.append(i)
+    print(uniq_list)
+
+
+unic_elem_2(my_list)
