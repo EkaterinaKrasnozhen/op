2a5ffdfa657bb7668f326214de417f45d548d177
kate <01redcat@mail.ru>
2022-10-15 20:47:31 +0300

    lec4 температура давление.. получение данных xml html

--- Python/Lections/lec4/task1/logger.py
+++ Python/Lections/lec4/task1/logger.py
@@ -4,17 +4,17 @@ from time import time
 def temperature_logger(data):
     time = dt.now().strftime('%H:%M')
     with open('log.cvs', 'a') as file:
-        file.write('{}; temperature; {}'
+        file.write('{}; temperature; {}\n'
                    .format(time, data))
         
 def pressure_logger(data):
     time = dt.now().strftime('%H:%M')
     with open('log.cvs', 'a') as file:
-        file.write('{}; pressure; {}'
+        file.write('{}; pressure; {}\n'
                    .format(time, data))
         
 def wind_speed_logger(data):
     time = dt.now().strftime('%H:%M')
     with open('log.cvs', 'a') as file:
-        file.write('{};wind speed; {}'
+        file.write('{};wind speed; {}\n'
                    .format(time, data))
