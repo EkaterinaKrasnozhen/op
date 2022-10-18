1404c70ef822e434a700d25eeca71297d80ad6f9
kate <01redcat@mail.ru>
2022-10-18 13:52:15 +0300

    h/w seminar 7 телефонный справочник todo

--- Python/homework_seminar7/controller_tele.py
+++ Python/homework_seminar7/controller_tele.py
@@ -1,5 +1,14 @@
 import user_view as uv
+import menu
 
-uv.new_pers()
 
-uv.find_someone()
+
+def start(number):
+ if number == 1:
+  uv.new_pers()
+ elif number == 2:
+  uv.find_someone()
+ elif number == 3:
+     return 3
+  
+
--- Python/homework_seminar7/model_tele.py
+++
@@ -1,5 +0,0 @@
-def get_new_person():
-    dict_persons = {input('Введите ФИО:'): input('Введите телефон:')}
-    return dict_persons
-    
-        
