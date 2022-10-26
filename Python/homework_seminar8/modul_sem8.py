b29427e6619fd2c73f3847069f3552848160b9a0
kate <01redcat@mail.ru>
2022-10-26 11:24:03 +0300

    h/w seminar 8 data baze moduls done

--- Python/homework_seminar8/modul_sem8.py
+++ Python/homework_seminar8/modul_sem8.py
@@ -11,24 +11,26 @@ def find_id():
     one= cursor.fetchone()
     print(*one)
     
-
+# найти по фамилии
 def find_last_name():
     last_n = us.search_surname()
     cursor.execute(f'SELECT * FROM  personal WHERE last_name="{last_n}";')
     one= cursor.fetchmany()
     print(*one)
     
-
+# удалить по id
 def delete_data():
     choice = us.delete_id()
     cursor.execute(f'DELETE FROM personal WHERE id={choice};')
     bd.commit()
-    
+
+# обновить данные оклада по id    
 def update_personal():
     id_sal = us.update_salary()
     cursor.execute(f'UPDATE personal SET salary = {int(id_sal[1])} WHERE id={id_sal[0]};')
     bd.commit()
-    
+
+# распечатать всю базу    
 def print_baza():
     for i in cursor.execute('SELECT * FROM personal'):
         print(*i)
