e1c8aed4e5869db2410d321d4fadb6ff79bed729
kate <01redcat@mail.ru>
2022-12-21 21:31:52 +0300

    Игра продолжение, abstract, static

---
+++ OOP/lec2/Teams.java
@@ -0,0 +1,27 @@
+package lec2;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Teams {
+    public static void main(String[] args) {
+        
+        
+
+        // System.out.printf("maigicalCount: %d priestCount: %d \n", magicalCount, priestCount);
+
+        // Priest[] priests = new Priest[priestCount];
+        // Magician[] magicians = new Magician[magicalCount];
+
+        // for (int i = 0; i < priestCount; i++) {
+        //     priests[i] = new Priest();
+        //     System.out.println(priests[i].getInfo());
+        // }
+        // System.out.println();
+
+        // for (int j = 0; j < magicalCount; j++) {
+        //     magicians[j] = new Magician();
+        //     System.out.println(magicians[j].getInfo());
+        // }
+    }
+}
