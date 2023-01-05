3af971a5846cea4561f605e775b5e94e18e5f061
kate <01redcat@mail.ru>
2023-01-05 19:17:40 +0300

    Serializeble save/read added, Enum Gender edded, ошибки исправлены

--- OOP/homeWorkSem1/Program.java
+++ OOP/homeWorkSem1/Program.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        Human human1 = new Human("Irina", 71, false, null, null);
-        Human human2 = new Human("Leonid", 69, true, null, null);
-        Human human3 = new Human("Liudmila", 62, false, null, null);
-        Human human4 = new Human("Alexander", 65, true,null, null);
-        Human human5 = new Human("Kate", 36, false, human2, human1);
-        Human human6 = new Human("Pavel", 37, true, human4, human3);
-        Human human7 = new Human("Yaroslav", 7, true, human6, human5);
-        Human human8 = new Human("Sviatoslav", 3, true, human6, human5);
+        Human human1 = new Human("Irina", 71, Gender.Female, null, null);
+        Human human2 = new Human("Leonid", 69, Gender.Male, null, null);
+        Human human3 = new Human("Liudmila", 62, Gender.Male, null, null);
+        Human human4 = new Human("Alexander", 65, Gender.Male,null, null);
+        Human human5 = new Human("Kate", 36, Gender.Female, human2, human1);
+        Human human6 = new Human("Pavel", 37, Gender.Male, human4, human3);
+        Human human7 = new Human("Yaroslav", 7, Gender.Male, human6, human5);
+        Human human8 = new Human("Sviatoslav", 3, Gender.Male, human6, human5);
 
         FamilyTree tree = new FamilyTree();
         tree.add(human1);
@@ -37,9 +37,9 @@ public class Program {
                 String name = iScanner.nextLine();
                 System.out.println("Возраст: ");
                 int age = iScanner.nextInt();
-                System.out.println("Введите пол М - true или Ж - false: ");
-                Boolean gender = iScanner.nextBoolean();
-                Human human9 = new Human(name, age, gender, null, null);//чтобы не вводить сразу много данных
+                System.out.println("Введите пол Male или Female: ");
+                String gender = iScanner.nextLine();
+                Human human9 = new Human(name, age, Gender.valueOf(gender), null, null);//чтобы не вводить сразу много данных
                 tree.add(human9);                                             //отца и мать можно добавить потом
             }
             if (get_user.equals("2")) {
