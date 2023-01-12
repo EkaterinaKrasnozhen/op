6d2bb0081e4800f6ca1fb5d69e05fc38b6e9440f
kate <01redcat@mail.ru>
2023-01-12 15:28:36 +0300

    mvp

---
+++ OOP/lec5/CalcModel.java
@@ -0,0 +1,5 @@
+package lec5;
+
+public abstract class CalcModel implements Model {
+    int x, y;
+}
---
+++ OOP/lec5/Model.java
@@ -0,0 +1,8 @@
+package lec5;
+
+public interface Model {
+    int result();
+    void setX(int value);
+    void setY(int value);
+
+}
