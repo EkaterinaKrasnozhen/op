73e0a33246432160864a924695e2c9b18729c61c
kate <01redcat@mail.ru>
2023-01-13 15:49:04 +0300

    Пример лекция 5 Записная книга MVP

---
+++ OOP/lec5/ExPhoneBook/Client/Program.java
@@ -0,0 +1,11 @@
+package lec5.ExPhoneBook.Client;
+
+import lec5.ExPhoneBook.UI.App;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        App.ButtonClick();
+     
+    }
+}
