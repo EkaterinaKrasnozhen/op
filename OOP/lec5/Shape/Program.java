5c46d2de356bc240dd4431160456145da3e4edd8
kate <01redcat@mail.ru>
2023-01-17 15:09:54 +0300

    фигуры

---
+++ OOP/lec5/Shape/Program.java
@@ -0,0 +1,31 @@
+package lec5.Shape;
+
+import lec5.Shape.Client.App;
+
+public class Program {
+    public static void main(String[] args) throws UnacceptableValueExcetion {
+
+        new App().start();
+        // Shape shape1 = Circle.create(1, "Circle1");
+        // Shape shape4 = Circle.create(2, "Circle2");
+        // Shape shape2 = Rectangle.create(2, 3, "Rectangle1");
+        // Shape shape3 = Rectangle.create(5, 6, "Rectangle2");
+        // Shape shape5 = Square.create(4, "Square1");
+
+
+        // Canvas canva =  new Canvas("Canvas");
+        // // canva.shapes.add(shape1);
+        // canva.shapes.add(shape2);
+        // canva.shapes.add(shape3);
+        // canva.shapes.add(shape4);
+        // canva.shapes.add(shape5);
+
+    //     shape3.shapes.add(shape2);
+    //     shape4.shapes.add(shape3);
+    //     shape4.shapes.add(shape1);
+    //     shape5.shapes.add(shape4);
+    //     canva.shapes.add(shape5);
+
+    //     System.out.println(canva);
+    }
+}
