5d401bc8947898fa80b2ef00b40bd605ebe580a8
kate <01redcat@mail.ru>
2023-01-29 15:18:06 +0300

    тут все врмя скачивать gson

--- OOP/seminar4/weather_api/Formatter.java
+++ OOP/seminar4/weather_api/Formatter.java
@@ -1,6 +1,6 @@
 package seminar4.weather_api;
 
-import com.google.gson.Gson;
+//import com.google.gson.Gson;
 
 import seminar4.weather_api.weather.Weather;
 
@@ -8,8 +8,8 @@ import seminar4.weather_api.weather.Weather;
 public class Formatter {
 
     public Weather parse(String info) {
-        Gson json = new Gson();
-        Weather weather = json.fromJson(info, Weather.class);
-        return weather;
+        // Gson json = new Gson();
+        // Weather weather = json.fromJson(info, Weather.class);
+        return null;//weather;
     }
 }
